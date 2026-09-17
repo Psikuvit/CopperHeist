dbffacfb6c5204f0a6825949ad1efe9f470bcfc8
Psikuvit <thegengestar@gmail.com>
2026-09-17 03:28:05 +0100

    Chore: wire up plugin and add config/messages
    Constructs every manager/service, registers listeners and the /ch
    command tree in onEnable; config.yml holds match/loot/golem tuning.

--- src/main/java/me/psikuvit/copperHeist/CopperHeist.java
+++ src/main/java/me/psikuvit/copperHeist/CopperHeist.java
@@ -1,16 +1,87 @@
 package me.psikuvit.copperHeist;
 
+import me.psikuvit.copperHeist.arena.ArenaManager;
+import me.psikuvit.copperHeist.arena.ArenaResetter;
+import me.psikuvit.copperHeist.command.CopperHeistCommand;
+import me.psikuvit.copperHeist.game.GameManager;
+import me.psikuvit.copperHeist.golem.DeliveryGoal;
+import me.psikuvit.copperHeist.listener.ArenaProtectionListener;
+import me.psikuvit.copperHeist.listener.CombatListener;
+import me.psikuvit.copperHeist.listener.GolemInteractListener;
+import me.psikuvit.copperHeist.listener.LootListener;
+import me.psikuvit.copperHeist.listener.PlayerConnectionListener;
+import me.psikuvit.copperHeist.loot.LootItem;
+import me.psikuvit.copperHeist.loot.LootWeightService;
+import me.psikuvit.copperHeist.ui.MessageService;
+import me.psikuvit.copperHeist.ui.SidebarService;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CopperHeist extends JavaPlugin {
 
+    private ArenaManager arenaManager;
+    private ArenaResetter arenaResetter;
+    private GameManager gameManager;
+    private MessageService messageService;
+    private SidebarService sidebarService;
+    private LootWeightService lootWeightService;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+
+        LootItem.init(this);
+        DeliveryGoal.init(this);
+
+        arenaManager = new ArenaManager(this);
+        arenaResetter = new ArenaResetter();
+        gameManager = new GameManager(this);
+        messageService = new MessageService(this);
+        messageService.load();
+        sidebarService = new SidebarService();
+        lootWeightService = new LootWeightService(this);
+
+        arenaManager.loadAll();
+
+        getServer().getPluginManager().registerEvents(new PlayerConnectionListener(this), this);
+        getServer().getPluginManager().registerEvents(new CombatListener(this), this);
+        getServer().getPluginManager().registerEvents(new LootListener(this), this);
+        getServer().getPluginManager().registerEvents(new GolemInteractListener(this), this);
+        getServer().getPluginManager().registerEvents(new ArenaProtectionListener(this), this);
+
+        CopperHeistCommand.register(this);
+
+        getLogger().info("Copper Heist enabled - " + arenaManager.all().size() + " arena(s) loaded.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (gameManager == null) return;
+        for (var game : gameManager.all()) {
+            game.forceStop();
+        }
+    }
+
+    public ArenaManager getArenaManager() {
+        return arenaManager;
+    }
+
+    public ArenaResetter getArenaResetter() {
+        return arenaResetter;
+    }
+
+    public GameManager getGameManager() {
+        return gameManager;
+    }
+
+    public MessageService getMessageService() {
+        return messageService;
+    }
+
+    public SidebarService getSidebarService() {
+        return sidebarService;
+    }
+
+    public LootWeightService getLootWeightService() {
+        return lootWeightService;
     }
 }
